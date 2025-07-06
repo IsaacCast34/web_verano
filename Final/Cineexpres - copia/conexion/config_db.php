6d06587adf85489fdf6f7917fd7523749f471ba8
IsaacCast34 <98409270+IsaacCast34@users.noreply.github.com>
2025-07-05 19:46:10 -0600

    Delete Final/Cineexpres - copia directory

--- Final/Cineexpres - copia/conexion/config_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '123456');
-define('DATABASE', 'CineExpress');
-?>
--- Final/Cineexpres - copia/verificar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Verifica si el usuario ha iniciado sesión
-if (!isset($_SESSION['usuario']) || !isset($_SESSION['tipo'])) {
-    header("Location: login.php");
-    exit();
-}
-?>
